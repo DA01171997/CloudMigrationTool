09c07c54a992bf140303c8d1b930876ea0a44cb4
maxw21 <max.wilhoite@gmail.com>
2019-10-15 20:20:10 -0700

    Changed relative to absolute path.

--- BackEnd/crawler.py
+++ BackEnd/crawler.py
@@ -1,16 +1,16 @@
-
 import os
 
 def crawl(pwd='/'):
     crawl_cmd = 'du -a -h --time --max-depth=1 ' + pwd + ' 2>/dev/null'
+    path_cmd = 'pwd'
     directory_list = []
-    directory = os.popen(crawl_cmd).read()
-    directory_split = directory.split('\n')
-
-    for i in range(0, len(directory_split) - 2):
-        size, time, path = directory_split[i].split('\t')
+    abs_path = os.popen(path_cmd).read().split('\n')[0]
+    directory = os.popen(crawl_cmd).read().split('\n')
+    for i in range(0, len(directory) - 2):
+        size, time, path = directory[i].split('\t')
         split_path = path.split('/')
         name = split_path[len(split_path) - 1]
+        path = abs_path
         if os.path.isfile(path):
             file_type = 0
         else:
@@ -19,6 +19,7 @@ def crawl(pwd='/'):
     return directory_list
 
 if __name__ == "__main__":
-	print(crawl('/home/max/Desktop/454_Project'))
-	print(crawl('/home/max/Desktop/454_Project/test'))
+        print(crawl('/home/max/Desktop/454_Project'))
+        print(crawl('/home/max/Desktop/454_Project/test'))
+        print(crawl('.'))
 
