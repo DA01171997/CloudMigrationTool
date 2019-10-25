34772e4efc810b35c1c3f091b14dbbd0ecb0e663
duydo <duy.ado97@gmail.com>
2019-10-25 16:47:17 -0700

    fix copy test

--- Employee/testemployeecopy.sh
+++ Employee/testemployeecopy.sh
@@ -5,7 +5,7 @@ echo "--------------------------------------------------------------------------
 curl --verbose \
 	--request POST \
 	--header 'Content-Type: application/json' \
-	--data @testemployeecrawl.json \
+	--data @testemployeecopy.json \
 	http://13.59.63.33:5001/api/v1/cloud/employee/copy
 echo
 echo "----------------------------------------------------------------------------------"
