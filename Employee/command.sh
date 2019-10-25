17d1cfec29e1cca53eda4e4000b58289790542b6
duydo <duy.ado97@gmail.com>
2019-10-25 15:30:21 -0700

    update script

--- Employee/command.sh
+++ Employee/command.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 FLASK_APP=employee.py flask run -h 0.0.0.0 -p 5001
--- Employee/setup.sh
+++ Employee/setup.sh
@@ -1,5 +1,9 @@
-sudo apt-get --yes install python3-pip
-sudo apt --yes install ruby-foreman
+#!/bin/bash
+
+sudo apt-get install --yes python3-pip
+sudo apt install --yes ruby-foreman
+sudo apt install --yes  python3-flask
+pip3 install --user Flask python-dotenv
 pip3 install --user Flask-API python-dotenv
 pip3 install --user pugsql
 pip3 install --user flask-cors
