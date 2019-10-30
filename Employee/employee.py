f34acb474971b893091e684e00a773bfb9d7bd6d
Ubuntu <ubuntu@ip-172-31-22-131.us-east-2.compute.internal>
2019-10-30 02:29:19 +0000

    Re-add change from crawl

--- Employee/employee.py
+++ Employee/employee.py
@@ -14,19 +14,19 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 def crawl(pwd='/'):
     crawl_cmd = 'du -a -h --time --max-depth=1 ' + pwd + ' 2>/dev/null'
-    path_cmd = 'pwd'
     directory_list = []
-    abs_path = os.popen(path_cmd).read().split('\n')[0]
     directory = os.popen(crawl_cmd).read().split('\n')
     for i in range(0, len(directory) - 2):
         size, time, path = directory[i].split('\t')
         split_path = path.split('/')
         name = split_path[len(split_path) - 1]
-        path = abs_path + '/' + name
+        path = pwd + '/' + name
         if os.path.isfile(path):
             file_type = 0
-        else:
+        elif os.path.isdir(path):
             file_type = 1
+        else:
+            file_type = 2
         directory_list.append([name, size, file_type, time, path])
     return directory_list
 
