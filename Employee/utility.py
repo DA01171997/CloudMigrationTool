3b937d9acd4f3759d22ed5f62bca145a9580f3fb
maxw21 <max.wilhoite@gmail.com>
2019-11-21 15:24:44 -0800

    Update

--- Employee/utility.py
+++ Employee/utility.py
@@ -101,9 +101,9 @@ if __name__ == "__main__":
     destination_ip = '18.189.26.44'
     destination_user = 'ubuntu'
     if sys.argv[7] == '0':
-        copy_with_key_file(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+        copy(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
     elif sys.argv[7] == '1':
-        transfer_with_key_file(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+        transfer(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
     #transfer_with_key_file(source_path, destination_path, destination_ip, destination_user, recursive, priv_key)
 
 
